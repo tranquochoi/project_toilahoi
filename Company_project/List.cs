e68eeca63483ed26fd4dcda7c96e8caed32e33f5
Your Name <92442628+Kazuo01@users.noreply.github.com>
2021-11-11 20:19:01 +0700

    Company

---
+++ Company_project/List.cs
@@ -0,0 +1,43 @@
+using System;
+    class List
+    {
+        private Production_Staff[] _lPS;
+        private int _n;
+
+        public Production_Staff[] LPS{get => _lPS;}
+
+        public void PS_List()
+        {
+            Console.Write("So luong nhan vien van phong: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Production_Staff [] p = new Production_Staff[n];
+
+            for (int i=0; i<n; i++)
+            {
+                Console.WriteLine("Nhap nhan vien thu " +(i+1));
+                Console.WriteLine(" ========== ");
+                p[i] = new Production_Staff();
+                p[i].Input();           
+                p[i].Display();    
+            }
+            Production_Staff tl;
+            for (int i=0; i<n; i++)
+            {
+                for (int j=i+1; j<n; j++)
+                {
+                    if (p[i].Luong< p[j].Luong)
+                    {
+                        tl = p[i];
+                        p[i] = p[j];
+                        p[j] = tl;
+                    }
+                }
+            }   
+            Console.WriteLine(" ========== ");
+            Console.WriteLine("So tien luong giam dan cua nhan vien san xuat\n");
+            for (int i=0; i<n; i++)
+            {   
+                Console.WriteLine("Ho ten: {0}\nLuong nhan vien san xuat: {1:F3}\n", p[i].HoTen, p[i].Luong);
+            }   
+        }
+    }
---
+++ Company_project/Production_Staff.cs
@@ -0,0 +1,66 @@
+using System.Runtime.InteropServices;
+using System;
+
+    class Production_Staff: Staff
+    {
+        private double _luongCanBan;
+        private int _soSanPham;
+
+        public double luongCanBan
+        {
+            get => _luongCanBan;
+            set => _luongCanBan = value;
+        }
+
+        public int soSanPham
+        {
+            get => _soSanPham;
+            set => _soSanPham = value;
+        }
+
+        public Production_Staff(): base()
+        {}
+
+        public Production_Staff(string hoTen, string ngaySinh, double luong, double luongCanBan, int soSanPham): base(hoTen, ngaySinh, luong)
+        {
+            _luongCanBan = luongCanBan;
+            _soSanPham = soSanPham;
+        }
+
+        public Production_Staff(Production_Staff ps):base((Production_Staff) ps)
+        {
+            _luongCanBan = ps._luongCanBan;
+            _soSanPham = ps._soSanPham;
+        }
+
+        public void Input()
+        {
+            Console.Write("Nhap Ho Ten: ");
+            HoTen = Console.ReadLine();
+            Console.Write("Nhap ngay sinh: ");
+            NgaySinh = (Console.ReadLine());
+            Console.Write("Nhap luong can ban: ");
+            _luongCanBan = double.Parse(Console.ReadLine());
+            Console.Write("Nhap so san pham: ");
+            _soSanPham = int.Parse(Console.ReadLine());
+        }
+
+        public void Display()
+        {   
+            Console.WriteLine(" ========== ");
+            Console.WriteLine("Danh sach nhan vien");
+            Console.WriteLine("Ho ten: {0} - Ngay sinh: {1} - Luong can ban: {2} - So san pham: {3} - Tien luong: {4:F3}", HoTen, NgaySinh, _luongCanBan, _soSanPham, Luong);
+            Console.WriteLine(" ========== ");
+
+        }
+
+        public new double Luong
+        {
+            get => Convert.ToDouble((_luongCanBan + _soSanPham)*5.000);
+        }
+
+
+
+    
+    }
+
