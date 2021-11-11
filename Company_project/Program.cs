7ca904d7e1a99b887e5aebd7da31c0d5b508fed2
Your Name <92442628+Kazuo01@users.noreply.github.com>
2021-11-11 21:31:07 +0700

    Company

---
+++ Company_project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Company_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List l1 = new List();
+            l1.PS_List();
+        }
+    }
+}
