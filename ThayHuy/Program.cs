497786fffe849907327c3ea6e831fe8b053d14f3
Your Name <92442628+Kazuo01@users.noreply.github.com>
2021-11-20 09:43:07 +0700

    20_11

---
+++ ThayHuy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ThayHuy
+{
+    public class Program
+    {
+		static void Main()
+
+		{ Console.WriteLine("    Loading");
+			ConsoleUtility.WriteProgressBar(0);
+			for (var i = 0; i <= 100; ++i)
+			{
+				ConsoleUtility.WriteProgressBar(i,true);
+				System.Threading.Thread.Sleep(50);
+			}
+			Console.WriteLine();
+			ConsoleUtility.WriteProgress(0);
+			for (var i = 0; i <= 100; ++i)
+			{
+				ConsoleUtility.WriteProgress(i, true);
+				System.Threading.Thread.Sleep(50);
+			}
+            Console.WriteLine("Hello, Mr.Huy!");
+		}
+	}
+}
